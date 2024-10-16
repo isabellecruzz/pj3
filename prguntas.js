f9b816e1d78629bfb91902ee7ec52cd5119c19dc
isabellecruzz <160685968+isabellecruzz@users.noreply.github.com>
2024-10-15 21:56:25 -0300

    Add files via upload

---
+++ prguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    ' O que é JavaScript?',
+    'O javaScript é uma linguagem de Programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+     'O que é uma função?',
+    'Uma função é um bloco que executa alguma tarefa'
+)
